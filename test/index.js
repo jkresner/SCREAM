f40ac7ad780849008aac317f1d633d368a533f39
Jonathon Kresner <jkresner@gmail.com>
2016-05-29 15:35:03 +1000

    0.6.5-beta.1

--- test/index.js
+++ test/index.js
@@ -10,8 +10,9 @@ describe('Framework Tests', function() {
   // describe('Data TODO', require('./lib/specs/data'))
   // describe('Db TODO', require('./lib/specs/db'))
   // describe('Etc. TODO', require('./lib/specs/etc.'))
-  describe('Config', require('./lib/specs/config'))
-  describe('Fixture', require('./lib/specs/fixture'))
-  describe('Integration', require('./lib/specs/integration'))
+  describe('UTIL', require('./lib/specs/util.obj'))
+  // describe('Config', require('./lib/specs/config'))
+  // describe('Fixture', require('./lib/specs/fixture'))
+  // describe('Integration', require('./lib/specs/integration'))
 
 })
